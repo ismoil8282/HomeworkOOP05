7d4f7796af144e627bb1b75f139b591ca0e18bea
ismoil8282 <137428819+ismoil8282@users.noreply.github.com>
2023-08-16 15:36:50 +0300

    start

---
+++ HomeworkOOP05/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeworkOOP05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
