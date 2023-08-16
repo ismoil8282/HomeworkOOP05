df570bae805c783712d531fbad3715df590d03d6
ismoil8282 <137428819+ismoil8282@users.noreply.github.com>
2023-08-16 16:12:40 +0300

    task 2

--- GitHubOOP/GitHubOOP/Program.cs
+++ GitHubOOP/GitHubOOP/Program.cs
@@ -2,29 +2,8 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Rectangle rectangle = new Rectangle();
-            rectangle.Width = 4;
-            rectangle.Height = 5;
-            rectangle.DisplayInfo();
-
-        }
+        string nur = "Davlatshox";
 
     }
-    class Rectangle
-    {
-        public double Width { get; set; }
-        public double Height { get; set; }
-
-        public  double CalculateArea()
-        {
-            return Width * Height;
-        }
-        public void DisplayInfo()
-        {
-            Console.WriteLine($"Area of Rectangle : {CalculateArea()}");
-        }
-
-    }
+   
 }
